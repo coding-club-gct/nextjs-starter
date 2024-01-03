79795199824bb73cd9c83783147e360e92b26712
aprilia <joel.sr1024@gmail.com>
2024-01-03 19:07:24 +0530

    init

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,4 @@
-import type { Config } from 'tailwindcss'
+import { tailwindConfig } from './src/theme/src'
 
-const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
+const config = tailwindConfig
 export default config
